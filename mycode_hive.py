4ba2f91be42ba193a40d8641a6c01facacbd82ef
fnk93 <f.krueger1993@gmail.com>
2018-03-18 22:27:26 +0100

    Update mycode_hive.py
    Added basic config which stores randomly generated device_id

--- mycode_hive.py
+++ mycode_hive.py
@@ -1,6 +1,8 @@
 from api import API
 from qpyou import QPYOU
 from tools import rndDeviceId
+import json
+import random
 
 my_email=''
 my_hivelogin=''
@@ -8,6 +10,22 @@ my_password=''
 # Leave empty if you don't know device id
 device_id = ''
 device = ''
+data = {}
+
+try:
+    with open('config.json', 'r') as f:
+        data = json.load(f)
+        if data.get('device_id'):
+            device_id = data.get('device_id')
+        else:
+            device_id = random.randint(200000000, 300000000)
+            data.update({'device_id': device_id})
+except FileNotFoundError:
+    with open('config.json', 'w') as f:
+        data = {
+            'device_id': random.randint(200000000, 300000000)
+        }
+        f.write(json.dumps(data))
 
 uid,did,sessionkey,appId=QPYOU(device_id, device).hiveLogin(my_hivelogin,my_password)
 a=API(uid,did,my_hivelogin,my_email,sessionkey,device=device,app_id=appId)
