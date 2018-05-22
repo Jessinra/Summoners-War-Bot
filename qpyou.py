6fe3eded6a11fe8a99c6351a8a014cd40998574c
Florian <f.krueger1993@googlemail.com>
2018-05-22 10:16:08 +0200

    otp support from Mila

--- qpyou.py
+++ qpyou.py
@@ -5,6 +5,7 @@ import json
 import random
 import requests
 import socket
+import os
 import urllib3
 from Crypto.Cipher import AES
 from hashlib import md5
@@ -69,12 +70,12 @@ class Activeuser():
 
 class QPYOU(object):
     def __init__(self, did=None, phone=None):
-        self.s = requests.session()
+        self.s = requests.Session()
         self.s.verify = False
         if 'Admin-PC' == socket.gethostname():
             self.s.proxies.update({'http': 'http://127.0.0.1:8888', 'https': 'https://127.0.0.1:8888', })
         self.s.headers.update({'Content-Type': 'application/json', 'Accept-Language': 'en-gb',
-                               'User-Agent': 'SMON_Kr/3.8.5.38500 CFNetwork/808.2.16 Darwin/16.3.0'})
+                               'User-Agent': 'Summoners%20War/3.8.6.38600 CFNetwork/808.2.16 Darwin/16.3.0'})
         self.did = did
         self.guest_uid = None
         self.p1 = '{"hive_country":"EN","device_country":"EN","timezone":null,"language":"en","game_language":"en",' \
@@ -119,7 +120,7 @@ class QPYOU(object):
             user_agent = 'xxx'
             vend_id = rndDeviceId()
         else:
-            ad_id = rndDeviceId()
+            ad_id = '00000000-0000-0000-0000-000000000000'
             device = 'SM-G955F'
             appid = 'com.com2us.smon.normal.freefull.google.kr.android.common'
             native_ver = 'Hive+v.2.6.6'
@@ -129,22 +130,36 @@ class QPYOU(object):
                          'like Gecko) Version/4.0 Chrome/63.0.3239.111 Mobile Safari/537.36 '
             vend_id = rndDeviceId()
         self.s.cookies.update(
-            {'advertising_id': ad_id, 'appid': appid,
+            {'hive_config_language':'en_US', 'hive_config_nationality':'CH', 'inquiry_language':'en_US', 'advertising_id': ad_id, 'appid': appid,
              'device': device, 'did': str(random.randint(200000000, 300000000)) if not self.did else str(self.did),
              'native_version': native_ver, 'osversion': osversion, 'platform': platform,
-             'vendor_id': vend_id})
+             'vendor_id': vend_id, 'gameindex': '2623', 'hive_source': 'C'})
         self.registered()
-        self.s.post('https://hub.qpyou.cn/auth',
-                    data='{"hive_country":"EN","device_country":"en","timezone":null,"language":"en",'
+        r1 = self.s.post('https://hub.qpyou.cn/auth',
+                         data='{"hive_country":"CH","device_country":"en","timezone":null,"language":"en",'
                          '"game_language":"en","server_id":""}',
-                    allow_redirects=False)
-        data = {'id': user, 'password': '', 'dkagh': MD5(password.encode('utf-8'))}
-        self.s.get('https://hub.qpyou.cn/auth/recent_account')
+                         allow_redirects=False)
+        data = f'id={user}&password=&dkagh={MD5(password.encode('utf-8'))}'
+        self.s.get('https://hub.qpyou.cn/auth/recent_account', cookies=r1.cookies)
         rr = self.s.post('https://hub.qpyou.cn/auth/login_proc', data=data,
                          headers={'Content-Type': 'application/x-www-form-urlencoded',
                                   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
                                   'User-Agent': user_agent,
                                   'Referer': 'https://hub.qpyou.cn/auth/login'}, allow_redirects=False)
+        if '/otp/' in rr.headers['Location']:
+            print('Detected otp...')
+            self.s.get('https://hub.qpyou.cn/otp/main', 
+                       headers={'Origin': 'https://hub.qpyou.cn',
+                                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+                                'User-Agent': user_agent,
+                                'Accept-Language':'en-gb',
+                                'Referer':'https://hub.qpyou.cn/auth/recent_account'})
+            _udata = input('Open this: /otp/aes.html?mail=MYMAIL@MY.COM&code=123456 and paste the console log here')
+            if 'ct' not in _udata and 'iv' not in _udata and '"s"' not in _udata and '"d"' not in _udata:
+                print('Bad data')
+                exit(1)
+            self.s.post('https://hub.qpyou.cn/otp/verification', data=_udata)
+            rr = self.s.get('https://hub.qpyou.cn/otp/login')
         sss = rr.headers['Location'].split('&')
         sessionkey = sss[3].replace('sessionkey=', '')
         _did = sss[2].replace('did=', '')
